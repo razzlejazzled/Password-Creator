636ca9c468e0de8dc8841c6aee1f2e1c89174e74
RazzleJazzled <robertjgassner@gmail.com>
2020-08-06 09:50:08 -0700

    added changes

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var passWordLength = prompt("How many characters would you like your password to be?")
-if (passWordLength < 8 || passWordLength > 128) {
-  alert("Length must be between 8 and 128 characters")
-}
+var generateBtn = document.querySelector("#generate")
 
 
 
-
-
-if (passWordLength > 8 || passWordLength < 128) {
-  var upperCase = confirm("Would you like uppercase characters in you password?")
-  var lowercase = confirm("Would you like lowercase characters in you password?")
-  var num3rs = confirm("Would you like numbers in your password?")
-  var specialCase = confirm("Would you like special characters in your password?")
-}
-else if (!(upperCase || lowerCase || num3rs || specialCase)) {
-  alert("You must select at least one type of character")
-}
 
 // Write password to the #password input
 function writePassword() {
@@ -26,7 +11,61 @@ function writePassword() {
 
   passwordText.value = password;
 
+  function generatePassword() {
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numeric = "123456789"
+    var special = "!@#$%^&*()_+-="
+    var lowerCase = "qwertyuiopasdfghjklzxcvbnm"
+    var options = {};
+
+
+    //prompt for password length
+    options.length = parseInt((prompt("How long would you like your password to be?")))
+
+    //alert for if character value is not correct
+    if (options.length < 8 || options.length > 128 || isNaN(options.length)) {
+      alert("Incorrect Password Length. Password must be between 8 and 128 characters");
+      return "Invalid parameters";
+    }
+
+    options.upperCase = confirm("Would you like uppercase characters in you password?");
+    options.lowerCase = confirm("Would you like lowercase characters in you password?");
+    options.num3rs = confirm("Would you like numbers in your password?");
+    options.special = confirm("Would you like special characters in your password?");
+
+    function getRandomValue(str) {
+      return str[Math.floor(Math.random() * str.length)]
+    }
+    password = "";
+
+    for (var i = 0; i < options.length; i++) {
+      if (options.special) {
+        password += getRandomValue(special)
+      }
+
+      if (options.num3rs) {
+        password += getRandomValue(numeric)
+      }
+
+      if (options.upperCase) {
+        password += getRandomValue(upperCase)
+      }
+
+      if (options.lowerCase) {
+        password += getRandomValue(lowerCase)
+
+      }
+
+
+    }
+    return password.substring(0, options.length);
+
+  }
 }
 
+
+
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
